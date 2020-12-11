3a50d799d9e64b6c52be263218623966d9c8498f
Marrione Nguyen <mtiamsic@gmail.com>
2020-12-10 22:34:56 -0800

    add for ask bcs

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import Nav from './components/Nav';
+import React, { useState } from 'react';
 import About from './components/About';
-import Gallery from "./components/Gallery";
+import Nav from './components/Nav';
+import Gallery from './components/Gallery';
 
 function App() {
   const [categories] = useState([
     {
       name: 'commercial',
-      description: "Photos of grocery stores, food trucks, and other commercial projects"
+      description: 'Photos of grocery stores, food trucks, and other commercial projects',
     },
-    { name: 'portraits', description: 'Portraits of people in my life'},
-    { name: 'food', description: 'Delicious delicacies'},
-    { name: 'landscape', description: 'Fileds, farmhouses, waterfalls, and the beauty of nature'}
+    { name: 'portraits', description: 'Portraits of people in my life' },
+    { name: 'food', description: 'Delicious delicacies' },
+    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
   ]);
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
@@ -22,10 +22,12 @@ function App() {
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
-        ></Nav>
+      ></Nav>
       <main>
-        <Gallery></Gallery>
-        <About></About>
+        <div>
+          <Gallery currentCategory={currentCategory}></Gallery>
+          <About></About>
+        </div>
       </main>
     </div>
   );
